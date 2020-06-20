3a7d852ba6ba49e8b50371e882deb7e4c2d5326f
Mark <mksricard@gmail.com>
2020-06-19 20:40:04 -0700

    added veiws, serializer, urls

--- apps/bugtracker/views.py
+++ apps/bugtracker/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import generics, permissions, status
+from rest_framework_tracking.mixins import LoggingMixin
+from .models import Bug
+from .serializers import BugSerializer
 # Create your views here.
+
+class BugListView(generics.ListAPIView):
+    queryset = Bug.objects.all()
+    serializer_class = BugSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
